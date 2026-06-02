6345e8a323cc9a9342cb8bdca9ff5e524ca60631
and1ru <andres.vanegas2700@gmail.com>
2026-06-02 14:10:06 -0500

    se configuro las rutas

--- src/routes/app-router.tsx
+++ src/routes/app-router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "../pages/home-page";
 
 export const routes = createBrowserRouter([
     {
-        
+        path: "",
+        element: <HomePage/>
     }
 ])
